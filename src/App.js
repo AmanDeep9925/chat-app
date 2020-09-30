8295a0a6480b087cd2ae8000af777ea8cfa84769
AmanDeep9925 <amandeep58257@gmail.com>
2020-10-01 01:16:26 +0530

    added new Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyBGtOtwv7h88BCQ0CMQGJoaKnKqpDhsXMY',
+    authDomain: 'chat-app-8f537.firebaseapp.com',
+    databaseURL: 'https://chat-app-8f537.firebaseio.com',
+    projectId: 'chat-app-8f537',
+    storageBucket: 'chat-app-8f537.appspot.com',
+    messagingSenderId: '378489184541',
+    appId: '1:378489184541:web:089fc2749f428397ec41f4',
+    measurementId: 'G-DDDWCRW4VH',
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
